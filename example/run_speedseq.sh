45abdf5f558b6a4cca606acd25bcc1e55793022e
EC2 Default User <ec2-user@ip-172-31-29-245.ec2.internal>
2015-03-14 18:08:21 +0000

    organized

--- example/run_speedseq.sh
+++ example/run_speedseq.sh
@@ -6,13 +6,13 @@ speedseq align \
     -M 3 \
     -p \
     -R "@RG\tID:NA12878\tSM:NA12878\tLB:lib1" \
-    human_g1k_v37_20_42220611-42542245.fasta \
-    NA12878.20slice.30X.fastq.gz
+    data/human_g1k_v37_20_42220611-42542245.fasta \
+    data/NA12878.20slice.30X.fastq.gz
 
 # 2. Detect SNVs and indels
 speedseq var \
     -o example \
-    human_g1k_v37_20_42220611-42542245.fasta \
+    data/human_g1k_v37_20_42220611-42542245.fasta \
     example.bam
 
 # 3. Detect SVs
@@ -21,4 +21,4 @@ speedseq sv \
     -B example.bam \
     -S example.splitters.bam \
     -D example.discordants.bam \
-    -R human_g1k_v37_20_42220611-42542245.fasta
+    -R data/human_g1k_v37_20_42220611-42542245.fasta
